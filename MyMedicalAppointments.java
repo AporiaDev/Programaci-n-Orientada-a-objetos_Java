ae67de1d92a73d68e69d82b63cccfe3f17f47b5c
AporiaDev <169740552+AporiaDev@users.noreply.github.com>
2025-06-28 22:59:00 -0500

    Summary

--- MyMedicalAppointments.java
+++ MyMedicalAppointments.java
@@ -1,11 +1,9 @@
 public class MyMedicalAppointments {
 
     public static void main(String[] args) {
-        Doctor doctor = new Doctor("Brayn", "cirujano");
-        doctor.showId();
-        Doctor doctor2 = new Doctor("Brenda", "cirujano");
-        doctor2.showId();
 
-        UIMenu.showMenu();
+
+        Patient patient = new Patient("Ale", "BBB@gmail.com");
+        System.out.println(patient);
     }
 }
