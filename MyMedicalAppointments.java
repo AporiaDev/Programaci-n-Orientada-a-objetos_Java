3a43e7a6a20c38d12e658e651d8d5959d71c97ab
AporiaDev <169740552+AporiaDev@users.noreply.github.com>
2025-07-01 21:44:43 -0500

    gg

--- MyMedicalAppointments.java
+++
@@ -1,9 +0,0 @@
-public class MyMedicalAppointments {
-
-    public static void main(String[] args) {
-
-
-        Patient patient = new Patient("Ale", "BBB@gmail.com");
-        System.out.println(patient); b
-    }
-}
